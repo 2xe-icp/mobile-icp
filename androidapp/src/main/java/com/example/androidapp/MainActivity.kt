fb1bad5eee464b20392652297d7c459166806a7f
Divya Jain <divyajain2405@gmail.com>
2019-07-15 21:24:07 -0700

    Changes for serialization

--- androidapp/src/main/java/com/example/androidapp/MainActivity.kt
+++ androidapp/src/main/java/com/example/androidapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.androidapp
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProviders
+import io.ktor.client.engine.okhttp.OkHttpConfig
+import io.ktor.client.engine.okhttp.OkHttpEngine
 import kotlinx.coroutines.*
 import sample.EventApi
 import kotlin.coroutines.CoroutineContext
@@ -21,13 +24,15 @@ class MainActivity : FragmentActivity(), CoroutineScope {
 
         val viewController = ViewModelProviders.of(this).get(MainViewController::class.java)
 
-        val eventApi = EventApi()
+
+        val eventApi = EventApi(OkHttpEngine(OkHttpConfig()))
         launch (Dispatchers.Main) {
             try {
                 val result = withContext(Dispatchers.IO) {
                     eventApi.fetchEvents()
                 }
-                Toast.makeText(this@MainActivity, result, Toast.LENGTH_LONG).show()
+
+                Toast.makeText(this@MainActivity, result.toString(), Toast.LENGTH_LONG).show()
             }
             catch (e: Exception) {
                 Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
--- core/src/commonMain/kotlin/EventApi.kt
+++ core/src/commonMain/kotlin/EventApi.kt
@@ -1,17 +1,32 @@
 package sample
 
 import io.ktor.client.*
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.request.get
 import io.ktor.client.request.url
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JSON
 
-class EventApi() {
-    private val client = HttpClient()
-    suspend fun fetchEvents(): String {
-        return client.get<String> {
+class EventApi(private val engine: HttpClientEngine) {
+    private val client by lazy {
+        HttpClient(engine) {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer(JSON.nonstrict)
+            }
+        }
+    }
+
+    suspend fun fetchEvents(): Event {
+        return client.get {
             url("$baseUrl")
         }
     }
     companion object {
         private const val baseUrl = "http://10.0.2.2:8080/"
     }
+
+    @Serializable
+    data class Event(val title: String, val description: String)
 }
